29db343742bcde6360d0514cd964c05d10c48094
Jesse Eichar <jesse.eichar@camptocamp.com>
2013-09-26 16:04:57 +0200

    initial commit.  basic project skeleton

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jesse
+ * Date: 9/26/13
+ * Time: 2:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
